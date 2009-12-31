d9cd6605a789cc34effbe94d842515d296f0944d
christian.nelson <christian.nelson@59c6d7f2-9b55-0410-a167-e7cfd95631ab>
2009-12-31 00:08:04 +0000

    Removed system.out statements.

--- src/test/java/com/carbonfive/test/functional/FunctionalTestRunnerTest.java
+++ src/test/java/com/carbonfive/test/functional/FunctionalTestRunnerTest.java
@@ -1,17 +1,15 @@
 package com.carbonfive.test.functional;
 
 import org.apache.commons.io.IOUtils;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static java.lang.String.format;
 import java.net.URL;
 import java.util.Properties;
+
+import static java.lang.String.format;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(FunctionalTestRunner.class)
 @FunctionalTestConfiguration("classpath:/com/carbonfive/test/functional/functional-tests.properties")
@@ -23,8 +21,6 @@ public class FunctionalTestRunnerTest
     @Test
     public void propertiesShouldBeSetByRunner()
     {
-        System.out.println("propertiesShouldBeSetByRunner()");
-
         assertThat(properties, not(nullValue()));
         assertThat(properties.size(), greaterThan(5));
     }
@@ -32,8 +28,6 @@ public class FunctionalTestRunnerTest
     @Test
     public void applicationShouldBeDeployed() throws Exception
     {
-        System.out.println("applicationShouldBeDeployed()");
-
         URL root = new URL(format("http://localhost:%s/", properties.getProperty("appserver.port")));
         String content = IOUtils.toString(root.openStream());
         assertThat(content, containsString("Orange"));
